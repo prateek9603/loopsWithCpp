f99d0ca2339a48a6e14f035bc88db8a6d06af412
prateek9603 <jain2k3.jp@gmail.com>
2024-02-26 18:36:38 +0530

    loops

---
+++ loops.cpp
@@ -0,0 +1,260 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+// q1 - Write a program to print the series 1,2,3,.....N...
+
+    // int n;
+    // cout<<"Enter the number of terms you want in the series: ";
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     cout<<i<<" , ";
+    // }
+
+
+// q2 - Write a program to print the first N terms of the series 1,-1,1,-1
+
+    // int n;
+    // cout<<"Enter the number of terms you want in the series: ";
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     int j=1;
+    //     if(i%2==0){
+    //         j=-1;
+    //     }
+    //     cout<<j<<" , ";
+    // }
+
+// q3 - Write a program to print the first N terms of the series 1,-3,5,-7,....
+
+    // int n;
+    // cout<<"Enter the number of terms you want in the series: ";
+    // cin>>n;
+
+    // int num=0;
+
+    // for(int i=1;i<=n;i++){
+    //     if(i%2==1){
+    //     ++num;
+    //         if(num%2==0){
+    //             cout<<i*(-1)<<" , ";
+    //             num=0;
+    //         }else cout<<i<<" , ";
+    //     }
+    // }
+
+// q4 -  Write a program to print the first N terms of the series 9,99,999...
+
+    // int n;
+    // cout<<"Enter the number of terms you want in the series: ";
+    // cin>>n;
+    //     string num;
+    // for(int i=1;i<=n;i++){        
+    //     num += "9" ;
+    //     cout<<num<<" , ";   
+    // }
+
+// q5 - Write a program to print the first N terms of the series 1,2,3,4,1,2,3,4,1,2,....
+
+    // int n;
+    // cout<<"Enter the number of terms you want in the series: ";
+    // cin>>n;
+
+    //     int num = 1;
+    // for(int i=1;i<=n;i++){
+    //     if(num<=4){
+    //         cout<<num<<",";
+    //         num++;
+    //     }else num=1;
+    // }
+
+
+// q6 - Fibonacci series
+
+    // int n;
+    // cout<<"Enter the number of terms you want in the series: ";
+    // cin>>n;
+
+    // int first = 0;
+    // int second = 1;
+
+    // cout<<first<<" , "<<second<<" , ";
+
+    // for(int i;i<=n-2;i++) {
+    //     int next=0;
+
+    //     next = first+second;
+    //     first=second;
+    //     second=next;
+
+    //     cout<<next<<" , ";
+    // }
+
+// q7 - tribonacci series 
+
+    // int n;
+    // cout<<"Enter the number of terms you want in the series: ";
+    // cin>>n;
+
+    // int first = 0;
+    // int second = 0;
+    // int third = 1;
+
+    // cout<<first<<" , "<<second<<" , "<<third<<" , ";
+
+    // for(int i;i<=n-4;i++) {
+    //     int next=0;
+
+    //     next = first+second+third;
+    //     first=second;
+    //     second=third;
+    //     third=next;
+
+    //     cout<<next<<" , ";
+    // }
+
+// q8 - Write a program to print pascal triangle. 
+// 1 
+// 1  1 
+// 1  2  1 
+// 1  3  3  1 
+// 1  4  6  4  1 
+// 1  5  10 10 5  1 
+
+    // int n;
+    // cout<<"Enter how many rows you want : ";
+    // cin>>n;
+
+    // int previous = 1;
+
+    // for(int i = 0;i<n;i++){
+    //     cout<<previous;
+    //     for(int j=1 ; j<=i ; j++){
+    //         previous = previous * (i-j+1)/j;
+    //         cout<<setw(3)<<previous;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+// -----------------------------pattern printing-----------------------------
+
+// --------1st--------
+
+    // int n;
+    // cout<<"Enter number of lines you want to print"<<endl;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     cout<<endl;
+    //     for(int j=1;j<=i;j++){
+    //         cout<<j;
+    //     }
+    // }
+
+    
+// --------2nd--------
+
+    // int n;
+    // cout<<"Enter number of lines you want to print"<<endl;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     cout<<endl;
+    //     for(int j=1;j<=i;j++){
+    //         cout<<i;
+    //     }
+    // }
+
+
+// --------3rd--------
+
+    // int n;
+    // cout<<"Enter number of lines you want to print"<<endl;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     cout<<endl;
+    //     for(int j=1;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    // }
+
+
+// --------4th--------
+
+    // int n;
+    // cout<<"Enter number of lines you want to print"<<endl;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     cout<<endl;
+    //     for(int j=i;j>=1;j--){
+    //         cout<<j;
+    //     }
+    // }
+
+
+// --------5th--------
+
+    // int n;
+    // cout<<"Enter number of lines you want to print"<<endl;
+    // cin>>n;
+
+    // for(int i=n;i>=1;i--){
+    //     cout<<endl;
+    //     for(int j=1;j<=i;j++){
+    //         cout<<j;
+    //     }
+    // }
+
+// --------6th--------
+
+    // int n;
+    // cout<<"Enter number of lines you want to print"<<endl;
+    // cin>>n;
+
+    // for(int i=n;i>=1;i--){
+    //     cout<<endl;
+    //     for(int j=i;j>=1;j--){
+    //         cout<<j;
+    //     }
+    // }
+
+// --------7th--------
+
+    // int n;
+    // cout<<"Enter number of lines you want to print"<<endl;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     char ch = 'a';
+    //         for(int j=1;j<=i;j++){
+    //             cout<<ch++;
+    //         }
+    //         cout<<endl;
+    // }
+
+
+// --------7th--------
+
+    // int n;
+    // cout<<"Enter number of lines you want to print"<<endl;
+    // cin>>n;
+
+    // for(int i=1;i<=n;i++){
+    //     char ch = 'a';
+    //         for(int j=1;j<=i;j++){
+    //             cout<<ch++;
+    //         }
+    //         cout<<endl;
+    // }
+    
+
+    
+}
+
